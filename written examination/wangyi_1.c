bc7f1255bb10a4ae444caa9b9d2f19b8e7077e92
zhoushiqi88 <zhoushiqi_uestc@yahoo.com>
2018-04-12 16:01:38 +0800

    待完成

---
+++ written examination/wangyi_1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main() {
+    int N;
+    int M;
+    scanf("%d",&N);
+    scanf("%d",&M);
+    int Di[100000];
+    int Pi[100000];
+    int Ai[100000];
+    int i=0;
+    while(i < &N) {
+        scanf("%d",&Di[i]);
+        scanf("%d",&Pi[i]);
+    }
+    int j=0;
+    while(j < &M) {
+        scanf("%d",&Ai[i]);
+    }
+}
